e31d701c4edc0f2fc115a07a60158ae279fd224c
Lalit1809 <jangidlalit1802@gmail.com>
2025-06-19 22:31:20 +0530

    Merged blog and polls apps into project

---
+++ blog/models.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser, User
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    state = models.CharField(null=True,blank=True)
+    city = models.CharField(null=True,blank=True)
+    country = models.CharField(null=True,blank=True)
+    image = models.ImageField(null=True,blank=True)
+
+    def __str__(self):
+        return self.email
---
+++ blog/views.py
@@ -0,0 +1,64 @@
+from .forms import PostForm
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post, User
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
+from django.contrib import messages
+
+# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def sign_up(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        state = request.POST.get('state')
+        city = request.POST.get('city')
+        country = request.POST.get('country')
+        image = request.FILES.get('image')
+
+        user = User.objects.create_user(username=username,email=email,password=password,image=image)
+        if user:
+            return redirect("post_list")
+
+    # messages.info(request,"Applications submited")
+
+    return render(request, 'blog/sign_up.html')
