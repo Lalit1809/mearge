442f3979d09dc830d17bfca613829089e8e8b226
Lalit1809 <jangidlalit1802@gmail.com>
2025-06-23 18:18:53 +0530

    update a post_list template

--- blog/views.py
+++ blog/views.py
@@ -41,11 +41,12 @@ def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     print(post,'got this post')
     if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
+        form = PostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
+            
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
